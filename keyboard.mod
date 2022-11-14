4d3290ef5466712d1b948270b505c64e26bf6b1c
sknjpn <sknjpn@gmail.com>
2022-11-14 22:17:09 +0900

    add auto generator

--- keyboard.mod
+++ keyboard.mod
@@ -1,17 +1,17 @@
-set POSITION;  # A-Z
-set KEY;
-param charset{KEY, KEY} default 0;
-param COSTMAP{POSITION, POSITION};
+set Position;  # A-Z
+set Key;
+param KeyMove{Key, Key} default 0;
+param PositionCost{Position, Position};
 
-var x {POSITION, KEY} binary;
+var x {Position, Key} binary;
 
 minimize COST:
-   sum {i in KEY, j in KEY, pi in POSITION, pj in POSITION}
-      x[pi, i] * x[pj, j] * COSTMAP[pi, pj] * charset[i, j];
+   sum {i in Key, j in Key, pi in Position, pj in Position}
+      x[pi, i] * x[pj, j] * PositionCost[pi, pj] * KeyMove[i, j];
 
-subject to KEYVALIDATION1{i in POSITION}:
-   sum {j in KEY} x[i, j] = 1;
+subject to KEYVALIDATION1{i in Position}:
+   sum {j in Key} x[i, j] = 1;
 
-subject to KEYVALIDATION2{i in KEY}:
-   sum {j in POSITION} x[j, i] = 1;
+subject to KEYVALIDATION2{i in Key}:
+   sum {j in Position} x[j, i] = 1;
  
--- script.py
+++ script.py
@@ -1,23 +1,58 @@
 import random
 
-for i in range(26):
-    for j in range(26):
-        print(chr(i + 65) +" "+chr(j + 65) +" "+ str(10 + random.randrange(100)))
+numberOfKeys = 26
+numberOfKeys = 4
+inputText = 'Learn from yesterday, live for today, hope for tomorrow.'
 
-text = 'Learn from yesterday, live for today, hope for tomorrow.'
+def generatePositionSet():
+    result = str()
+    for i in range(numberOfKeys):
+        result += chr(65 + i) + ' '
+    with open('data/positionSet.dat', 'w') as f:
+        f.write('set Position := ' + result[:-1] + ';')
 
-matrix = [[0] * 26 for i in range(26)]
-for t in range(len(text) - 1):
-    if ord(text[t]) < 97 or ord(text[t]) > 122:
-        continue
-    if ord(text[t+1]) < 97 or ord(text[t+1]) > 122:
-        continue
-    matrix[ord(text[t])-97][ord(text[t+1])- 97]+=1
-    print(text[t]+" "+ text[t+1])
+def generateKeySet():
+    result = str()
+    for i in range(numberOfKeys):
+        result += chr(97 + i) + ' '
+    with open('data/keySet.dat', 'w') as f:
+        f.write('set Key := ' + result[:-1] + ';')
 
-#print(matrix)
-#for i in range(26):
-#    for j in range(26):
-        #if matrix[i][j] >= 1:
-        #print(chr(i + 97) + " "+chr(j + 97)+" "+str(matrix[i][j]))
-        #print(chr(i + 97) + " "+chr(j + 97)+" "+str(random.randrange(100) + 10))
+def generateRandomPositionCost():
+    result = str()
+    for i in range(numberOfKeys):
+        for j in range(numberOfKeys):
+            result += "  "+chr(65+i) +" "+chr(65+j) +" "+ str(10 + random.randrange(100)) + '\n'
+    with open('data/positionCost.dat', 'w') as f:
+        f.write('param PositionCost :=\n' + result[:-1] + ';')
+
+def generateRandomKeyMove():
+    result = str()
+    for i in range(numberOfKeys):
+        for j in range(numberOfKeys):
+            result += "  "+chr(97+i) +" "+chr(97+j) +" "+ str(10 + random.randrange(100)) + '\n'
+    with open('data/keyMove.dat', 'w') as f:
+        f.write('param KeyMove :=\n' + result[:-1] + ';')
+
+def generateKeyMoveFrom(text = 'Learn from yesterday, live for today, hope for tomorrow.'):
+    result = str()
+    matrix = [[0] * 26 for i in range(26)]
+    for t in range(len(text) - 1):
+        if ord(text[t]) < 97 or ord(text[t]) > 97+numberOfKeys-1:
+            continue
+        if ord(text[t+1]) < 97 or ord(text[t+1]) > 97+numberOfKeys-1:
+            continue
+        matrix[ord(text[t])-97][ord(text[t+1])- 97]+=1
+    
+    for i in range(26):
+        for j in range(26):
+            if matrix[i][j] >= 1:
+                result += "  "+ chr(i + 97) + " "+chr(j + 97)+" "+str(matrix[i][j])+'\n'
+    with open('data/keyMove.dat', 'w') as f:
+        f.write('param KeyMove :=\n' + result[:-1] + ';')
+
+generatePositionSet()
+generateKeySet()
+generateRandomPositionCost()
+generateRandomKeyMove()
+#generateKeyMoveFrom()
